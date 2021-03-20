094e0708aa25707732713b3963e68155e8bc333c
Mateusz Choma <chomamateusz@gmail.com>
2021-03-20 12:39:54 +0100

    ✨ Working Directus custom interface

---
+++ src/interface.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+	<div>My Custom Interface</div>
+	<!-- Expects type string -->
+	<input v-model="value" @input="handleChange" />
+</div>
+</template>
+
+<script>
+export default {
+	props: {
+		value: String,
+	},
+	methods: {
+		handleChange() {
+			this.$emit('input', this.value);
+		},
+	},
+};
+</script>
